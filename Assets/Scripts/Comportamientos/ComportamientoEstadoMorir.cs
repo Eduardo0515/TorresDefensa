d343b68e27c9d0c7d8d38e16c96342a55a93ee92
Hugo150 <edurepe9@gmail.com>
2021-02-01 23:43:29 -0600

    First commit

---
+++ Assets/Scripts/Comportamientos/ComportamientoEstadoMorir.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComportamientoEstadoMorir : StateMachineBehaviour
+{
+    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        //Destruye el gameObject del estado
+        Destroy(animator.gameObject);
+    }
+}
